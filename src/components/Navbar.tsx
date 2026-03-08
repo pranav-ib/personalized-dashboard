20951535fe57fc71952977c8338597576fe7567f
Pranav I B <pranavbabunadhan@gmail.com>
2026-03-09 00:47:49 +0530

    fix:sidebar, header

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,14 +10,15 @@ export default function Navbar(){
     const query = useSelector((state: RootState) => state.search.query);
 
     return(
-        <div className="w-full h-16 bg-gray-800 text-white flex items-center justify-between px-4">
-            <h1 className="text-lg font-bold">My Dashboard</h1>
+        <div className="sticky top-0 z-50 w-[85%] mx-auto mt-8 bg-white rounded-full shadow-md px-8 py-4 flex items-center justify-between">
+            <h1 className="text-3xl font-bold text-purple-900">My Dashboard</h1>
             
             <input type="text" placeholder="Search content..."
                 value={query}
                 onChange={(e)=> dispatch(setQuery(e.target.value))}
-                className=" border px-4 py-2 rounded-lg"
-         />
+                className="px-5 py-2 rounded-full border border-gray-300 outline-none focus:ring-2 focus:ring-purple-400"
+            />
+            
         </div>            
     );
 }
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,33 @@
 "use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
+export default function Sidebar(){
+     const pathname = usePathname();
+
+    const linkStyle = (path: string) =>
+        `px-3 py-2 rounded-lg transition 
+        ${pathname === path 
+        ? "bg-purple-100 text-purple-700 font-semibold" 
+        : "text-gray-700 hover:bg-purple-100 hover:text-purple-700"}`;
+        
+    return(
+        <div className="flex gap-10 p-10">
+            <aside className="w-56 bg-gray-100 rounded-3xl shadow-md p-6">
+                <nav className="flex flex-col gap-3 text-gray-700 font-medium">
+                    <a href="/feed" className={linkStyle("/feed")}>Your Feed</a>
+                    <a href="/trending" className={linkStyle("/trending")}>Trending</a>
+                    <a href="/favorites" className={linkStyle("/favorites")}>Favorites</a>
+                    <a href="/preferences" className={linkStyle("/preferences")}>Preferences</a>
+                </nav>
+            </aside>
+        </div>
+    );
+}
+
+
+
+/*
 export default function Sidebar(){
     return(
         <div className="w-64 bg-white rounded-xl p-6 shadow-md">
@@ -13,3 +40,5 @@ export default function Sidebar(){
         </div>
     );
 }
+
+*/
